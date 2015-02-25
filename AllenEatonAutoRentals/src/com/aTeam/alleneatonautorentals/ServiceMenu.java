8bd4ea42e6dcc294297e4ea79744da1e2d871c78
thaismombach <thais.mombach37@hotmail.com>
2015-02-24 23:13:59 -0600

    Added Service Tablet Project

---
+++ AllenEatonAutoRentals/src/com/aTeam/alleneatonautorentals/ServiceMenu.java
@@ -0,0 +1,68 @@
+package com.aTeam.alleneatonautorentals;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ServiceMenu extends ActionBarActivity {
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_service_menu);		
+	}
+
+	
+	public void authenticateLogout(View view) {
+		
+        Intent i=new Intent(ServiceMenu.this, ServiceLogin.class);
+        startActivity(i);
+
+	}
+	
+	public void checkInCar(View view) {
+		
+		Intent i=new Intent(ServiceMenu.this, ServiceCheckIn.class);
+        startActivity(i);
+		
+	}
+		
+	public void viewCarsInUse(View view) {
+		
+		Intent i=new Intent(ServiceMenu.this, ServiceViewCars.class);
+        startActivity(i);
+		
+	}
+	
+	public void addNote(View view) {
+		
+		Intent i=new Intent(ServiceMenu.this, ServiceAddNote.class);
+        startActivity(i);
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.service_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
