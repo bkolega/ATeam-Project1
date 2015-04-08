4e7f9360549f4868cb30492bd88843b225ba2d79
David Young <roboguy06@gmail.com>
2015-04-08 12:30:11 -0500

    The customer login.php script is working.

--- AllenEatonRentals/src/CustomerPHPScripts/db_access.php
+++ AllenEatonRentals/src/CustomerPHPScripts/db_access.php
@@ -2,9 +2,9 @@
 	$file = file_get_contents("db_access.json");
 	$sql_json = json_decode($file, true);
 	
-	$c = mysqli_connect($sql_json["host"], $sql_json["user"], $sql_json["password"], $sql_json["db"]);
+	$conn = mysqli_connect($sql_json["host"], $sql_json["user"], $sql_json["password"], $sql_json["db"]);
 
-	if (mysqli_connect_errno($c)) {
+	if (mysqli_connect_errno($conn)) {
 	   echo "Failed to connect to MySQL" . mysqli_connect_error();
 	   exit;
 	}
--- AllenEatonRentals/src/CustomerPHPScripts/login.php
+++ AllenEatonRentals/src/CustomerPHPScripts/login.php
@@ -1,5 +1,6 @@
 <?php
-
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
 include "db_access.php";
 
 if (empty($_POST['username']) || empty($_POST['password'])) {
@@ -15,15 +16,21 @@ $query = $conn->prepare('SELECT E.user_email, user_password, customer_card_numbe
                             (SELECT *
                              FROM `ALLEN_EATON_AUTO.CUSTOMER`
                              WHERE user_email=?) E
-                         WHERE U.user_email=?)');
+                         WHERE U.user_email=?');
 
-$query->bind_param('ss', $_POST['username']);
+if (!$query) {
+  die('MySQL error: ' . $conn->error);
+}
+
+$query->bind_param('ss', $_POST['username'], $_POST['username']);
 
 $query->execute();
 
-$query->bind_result($login_array);
+$login_result = $query->get_result();
 
 $query->fetch();
+
+$login_array = $login_result->fetch_array(MYSQLI_ASSOC);
 
 if (!empty($login_array) && password_verify($_POST['password'], $login_array['user_password'])) {
   $response['success'] = 1;
